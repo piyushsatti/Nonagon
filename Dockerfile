7f1af4e168cdbb89da3f2430116bb76f160eeb6e
piyushsatti <piyushsatti@gmail.com>
2025-06-17 17:12:05 -0400

    Attempted resolve atlas certificate

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM python:3.11
 WORKDIR /root/nonagon
 COPY . .
+RUN apt-get update \
+ && apt-get install -y --no-install-recommends ca-certificates \
+ && rm -rf /var/lib/apt/lists/*
 RUN pip install -r requirements.txt
 CMD [ "python", "-m", "app.bot.main" ]
--- app/bot/database.py
+++ app/bot/database.py
@@ -2,11 +2,19 @@ from dataclasses import asdict, is_dataclass
 import logging
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import certifi
 
 from .config import DB_USERNAME, DB_PASSWORD
 
 uri = f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@nonagon.upiarrl.mongodb.net/?retryWrites=true&w=majority&appName=nonagon"
-db_client = MongoClient(uri, server_api=ServerApi('1'))
+# db_client = MongoClient(uri, server_api=ServerApi('1'))
+
+db_client = MongoClient(
+    uri,
+    tls=True,                        # enable TLS
+    tlsCAFile=certifi.where(),       # point at certifi’s Mozilla CA bundle
+    server_api=ServerApi('1')
+)
 
 try:
   db_client.admin.command('ping')
