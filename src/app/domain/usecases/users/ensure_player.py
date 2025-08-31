396ebbf52735cc49021cdb233f1bf25b9230c905
piyushsatti <piyushsatti@gmail.com>
2025-08-31 09:49:04 -0400

    Redoing usecases

--- src/app/domain/usecases/users/ensure_player.py
+++
@@ -1,7 +0,0 @@
-from app.domain.models.UserModel import UserId
-
-async def ensure_player(users_repo, user_id: UserId):
-  u = await users_repo.get(user_id)
-  u.enable_player()
-  await users_repo.upsert(u)
-  return u
