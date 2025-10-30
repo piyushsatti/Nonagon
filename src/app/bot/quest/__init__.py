cb01f73d729d2f1c4bf0e6aadc79d8fdb785a1ce
Piyush Satti <piyushsatti@gmail.com>
2025-10-29 23:01:05 -0400

    Refactor quest and character command internals into dedicated modules

--- src/app/bot/quest/__init__.py
+++ src/app/bot/quest/__init__.py
@@ -1,1 +1,39 @@
 """Quest-related helpers for bot commands."""
+
+from .commands import (
+    quest_announce,
+    quest_cancel,
+    quest_create,
+    quest_edit,
+    quest_nudge,
+    quest_players,
+)
+from .embeds import build_nudge_embed, build_quest_embed, quest_to_embed_data
+from .sessions import (
+    QuestAnnounceView,
+    QuestConfirmView,
+    QuestCreationResult,
+    QuestCreationSession,
+    QuestSessionBase,
+    QuestUpdateResult,
+    QuestUpdateSession,
+)
+
+__all__ = [
+    "build_nudge_embed",
+    "build_quest_embed",
+    "quest_to_embed_data",
+    "QuestAnnounceView",
+    "QuestConfirmView",
+    "QuestCreationResult",
+    "QuestCreationSession",
+    "QuestSessionBase",
+    "QuestUpdateResult",
+    "QuestUpdateSession",
+    "quest_announce",
+    "quest_cancel",
+    "quest_create",
+    "quest_edit",
+    "quest_nudge",
+    "quest_players",
+]
